1e13cc6845af250b0ca100c6fb5595b10942d673
duxet <duxetlg@gmail.com>
2024-04-18 14:38:07 +0200

    Add note about configs

--- postinstall.sh
+++ postinstall.sh
@@ -1,3 +1,6 @@
+# add configs to /etc/config manually (luci, system, network and rpcd at least)
+# TODO: add default configs to each package
+
 systemctl enable --now logd.service
 systemctl enable --now ubus.socket
 systemctl enable --now netifd.service
